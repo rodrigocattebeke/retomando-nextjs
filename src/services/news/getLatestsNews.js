330158b7589fd4f9572332cb9dac57d7753d1277
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-12-23 16:57:26 -0300

    feat: update services

--- src/services/news/getLatestsNews.js
+++ src/services/news/getLatestsNews.js
@@ -1,6 +1,12 @@
-export const getLatestsNews = async () => {
+export const getLatestsNews = async ({ page } = {}) => {
+  const BASE_URL = "http://localhost:3000/api/news";
+  let url = BASE_URL;
+
+  if (page) {
+    url += `?page=${page}`;
+  }
   try {
-    const res = await fetch("http://localhost:3000/api/news");
+    const res = await fetch(url);
     const data = await res.json();
     return data;
   } catch (error) {
--- src/services/news/getSingleNews.js
+++ src/services/news/getSingleNews.js
@@ -1,5 +1,11 @@
 import news from "@/dev/news.json";
 
-export const getSingleNews = () => {
-  return news;
+export const getSingleNews = async (newsId) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/news?id=${newsId}`);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    return { success: false, error };
+  }
 };
